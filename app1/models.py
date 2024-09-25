aa7c569784e9b648bd07252e48dcae5610fa35fa
paexternal229 <paexternal229@oaisb.info>
2024-09-25 20:48:48 +0500

    Added Email Field

--- app1/models.py
+++ app1/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=250)
-    email = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
     
