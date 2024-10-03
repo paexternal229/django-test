aa570038c3cc852f5cce3b0e317098a2d6803f70
paexternal229 <paexternal229@oaisb.info>
2024-10-03 04:41:45 -0700

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -10,3 +10,10 @@ def students(request):
         students_list  = [student.name + '\n' for student in all_students]
 
         return HttpResponse(students_list)
+
+def teachers(request):  # New view for Teacher model
+        if request.method == 'GET':
+                    all_teachers = Teacher.objects.all()
+                    teachers_list = [teacher.name + ' - ' + teacher.subject + '\n' for teacher in all_teachers]
+                    return HttpResponse(teachers_list)
+            
