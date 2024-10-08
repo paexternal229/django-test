371003d0d8c48e56fc4f20f731d812078f1d1601
paexternal229 <paexternal229@oaisb.info>
2024-10-08 06:25:48 -0700

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -10,3 +10,12 @@ class Student(models.Model):
 class Teacher(models.Model):
     name = models.CharField(max_length=250)
     subject = models.CharField(max_length=250, null=True)
+
+class Course(models.Model):
+        title = models.CharField(max_length=250)
+        description = models.TextField(null=True, blank=True)
+        teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+        students = models.ManyToManyField(Student, related_name='courses')
+
+    def __str__(self):
+                return self.title
